2d90c240d1b13190d0bf0d75f36421ca2a58db69
Kevin Harvell <kevharvell@gmail.com>
2019-04-19 12:37:22 -0500

    refactor cardEffect w playVillage function

--- projects/harvellk/dominion/dominion.c
+++ projects/harvellk/dominion/dominion.c
@@ -748,6 +748,20 @@ int playSmithy(struct gameState *state, int handPos) {
 	return 0;
 }
 
+int playVillage(struct gameState *state) {
+	int currentPlayer = whoseTurn(state);
+
+	//+1 Card
+	drawCard(currentPlayer, state);
+
+	//+2 Actions
+	state->numActions = state->numActions + 2;
+
+	//discard played card from hand
+	discardCard(handPos, currentPlayer, state, 0);
+	return 0;
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
 	int i;
@@ -878,16 +892,9 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 		
 
 	case village:
-		//+1 Card
-		drawCard(currentPlayer, state);
-
-		//+2 Actions
-		state->numActions = state->numActions + 2;
-
-		//discard played card from hand
-		discardCard(handPos, currentPlayer, state, 0);
-		return 0;
-
+		playVillage(state);
+		break;
+		
 	case baron:
 		state->numBuys++;//Increase buys by 1!
 		if (choice1 > 0) {//Boolean true or going to discard an estate
