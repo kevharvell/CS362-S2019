5cc1645999615f79f8805f78594a300be298f295
Kevin Harvell <kevharvell@gmail.com>
2019-04-26 18:58:20 -0500

    testing first unit test

---
+++ projects/harvellk/dominion/unittest1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <assert.h>
+
+//int numHandCards(struct gameState *state) {
+//	return state->handCount[whoseTurn(state)];
+//}
+
+void testNumHandCards() {
+	struct gameState state;
+	state.whoseTurn = 0;
+	state.handCount[state.whoseTurn] = 5;
+
+
+	assert(numHandCards(state) == 5);
+}
+
+int main() {
+	testNumHandCards();
+	printf("\n >>>>> SUCCESS: Testing complete <<<<<\n\n");
+	return 0;
+}
+
