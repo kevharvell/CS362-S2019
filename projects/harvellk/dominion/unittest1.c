e30ff4e354fb2bc7b2e37cf262172c9604daa1b7
Kevin Harvell <kevharvell@gmail.com>
2019-04-29 12:59:23 -0500

    fixed unit test 1 to test playVillage

--- projects/harvellk/dominion/unittest1.c
+++ projects/harvellk/dominion/unittest1.c
@@ -8,139 +8,52 @@
 #include <assert.h>
 #include <limits.h>
 
+/*int playVillage(struct gameState *state, int handPos) {
+	int currentPlayer = whoseTurn(state);
+
+	//+1 Card
+	drawCard(currentPlayer, state);
+
+	//+2 Actions
+	state->numActions = state->numActions + 3;
+
+	//discard played card from hand
+	discardCard(handPos, currentPlayer, state, 0);
+	return 0;
+}*/
+
 void assertTrue(int expression, char message[]) {
 	if (!expression) {
 		printf(message);
 	}
 }
 
-void testNumHandCards() {
+void testPlayVillage() {
 	struct gameState *state = malloc(sizeof(struct gameState));
-
-	// TEST 1: players have correct number of cards
-	printf("\n >>> TESTING - numHandCards(gameState*) <<<\n");
-	printf(" Number of Cards in all players' hands is correct\n");
-	int i;
-	int numCards = 1; 
-	for (i = 0; i < MAX_PLAYERS; i++) {
-		state->whoseTurn = i;
-		state->handCount[state->whoseTurn] = numCards;
-		printf("Player: %d \tExpected Number of Cards in hand: %d \tActual Number of Cards in hand: %d\n",
-			state->whoseTurn,
-			numCards,
-			numHandCards(state));
-		int expectedNumCards = numHandCards(state);
-		assertTrue(expectedNumCards == numCards, "TEST FAILED: hand count incorrect.\n");
-		numCards++;
-	}
-
-	// TEST 2: players have correct number of cards after gaining cards
-	printf("\n >>> TESTING - numHandCards(gameState*) <<<\n");
-	printf(" Number of Cards in all players' hands is correct after gaining 2 cards.\n");
-	numCards = 1;
-	for (i = 0; i < MAX_PLAYERS; i++) {
-		state->whoseTurn = i;
-		state->handCount[state->whoseTurn] = numCards;
-		state->handCount[state->whoseTurn] += 2;
-		numCards += 2;
-		printf("Player: %d \tExpected Number of Cards in hand: %d \tActual Number of Cards in hand: %d\n",
-			state->whoseTurn,
-			numCards,
-			numHandCards(state));
-		int expectedNumCards = numHandCards(state);
-		assertTrue(expectedNumCards == numCards, "TEST FAILED: hand count incorrect after gaining 2 cards.");
-		numCards++;
-	}
 	
-	// TEST 3: players have correct number of cards after losing cards
-	printf("\n >>> TESTING - numHandCards(gameState*) <<<\n");
-	printf(" Number of Cards in all players' hands is correct after losing 2 cards.\n");
-	numCards = 3;
-	for (i = 0; i < MAX_PLAYERS; i++) {
-		state->whoseTurn = i;
-		state->handCount[state->whoseTurn] = numCards;
-		state->handCount[state->whoseTurn] -= 2;
-		numCards -= 2;
-		printf("Player: %d \tExpected Number of Cards in hand: %d \tActual Number of Cards in hand: %d\n",
-			state->whoseTurn,
-			numCards,
-			numHandCards(state));
-		int expectedNumCards = numHandCards(state);
-		assertTrue(expectedNumCards == numCards, "TEST FAILED : hand count incorrect after losing 2 cards.");
-		numCards+=3;
-	}
+	// TEST 1: players have correct number of cards
+	printf("\n >>> TESTING - playVillage(gameState*, int) <<<\n");
+	printf(" playVillage increases hand count by 1\n");
+	int handPos = 0;
+	state->whoseTurn = 0;
+	int currentPlayer = state->whoseTurn;
+	state->handCount[currentPlayer] = 5;
+	int expectedHandCount = 6;
 
-	// TEST 4: players can gain a lot of cards
-	printf("\n >>> TESTING - numHandCards(gameState*) <<<\n");
-	printf(" Number of Cards in all players' hands is correct after gaining a lot of cards.\n");
-	numCards = 0;
-	for (i = 0; i < MAX_PLAYERS; i++) {
-		state->whoseTurn = i;
-		state->handCount[state->whoseTurn] = numCards;
-		state->handCount[state->whoseTurn] += INT_MAX;
-		printf("Player: %d \tExpected Number of Cards in hand: %d \tActual Number of Cards in hand: %d\n",
-			state->whoseTurn,
-			INT_MAX,
-			numHandCards(state));
-		int expectedNumCards = numHandCards(state);
-		assertTrue(expectedNumCards == INT_MAX, "TEST FAILED: hand count incorrect after gaining a lot cards.");
-	}
+	playVillage(state, handPos);
+	int actualHandCount = state->handCount[currentPlayer];
 
-	// TEST 5: players can lose a lot of cards
-	printf("\n >>> TESTING - numHandCards(gameState*) <<<\n");
-	printf(" Number of Cards in all players' hands is correct after losing a lot of cards.\n");
-	numCards = 0;
-	for (i = 0; i < MAX_PLAYERS; i++) {
-		state->whoseTurn = i;
-		state->handCount[state->whoseTurn] = INT_MAX;
-		state->handCount[state->whoseTurn] -= INT_MAX;
-		printf("Player: %d \tExpected Number of Cards in hand: %d \tActual Number of Cards in hand: %d\n",
-			state->whoseTurn,
-			0,
-			numHandCards(state));
-		int expectedNumCards = numHandCards(state);
-		assertTrue(expectedNumCards == 0, "TEST FAILED: hand count incorrect after losing a lot of cards.");
-	}
-
-	// TEST 6: number of cards in hand should not be negative
-	printf("\n >>> TESTING - numHandCards(gameState*) <<<\n");
-	printf(" Number of Cards in all players' hands should not be negative.\n");
-	numCards = 10;
-	for (i = 0; i < MAX_PLAYERS; i++) {
-		state->whoseTurn = i;
-		state->handCount[state->whoseTurn] = numCards;
-		state->handCount[state->whoseTurn] -= INT_MAX;
-		numCards -= INT_MAX;
-		printf("Player: %d \tExpected Number of Cards in hand: %d \tActual Number of Cards in hand: %d\n",
-			state->whoseTurn,
-			numCards,
-			numHandCards(state));
-		int expectedNumCards = numHandCards(state);
-		assertTrue(expectedNumCards >= 0, "TEST FAILED: Hand count should not be negative.\n");
-		numCards += INT_MAX;
-		numCards++;
-	}
-
-	// TEST 7: number of cards in hand can be 0
-	printf("\n >>> TESTING - numHandCards(gameState*) <<<\n");
-	printf(" Number of Cards in all players' hands can be 0.\n");
-	numCards = 0;
-	for (i = 0; i < MAX_PLAYERS; i++) {
-		state->whoseTurn = i;
-		state->handCount[state->whoseTurn] = numCards;
-		printf("Player: %d \tExpected Number of Cards in hand: %d \tActual Number of Cards in hand: %d\n",
-			state->whoseTurn,
-			numCards,
-			numHandCards(state));
-		int expectedNumCards = numHandCards(state);
-		assertTrue(expectedNumCards == numCards, "TEST FAILED: Hand count not allowed to be 0.\n");
-	}
+	printf("Expected number of cards in hand: %d \tActual number of cards in hand: %d",
+			expectedHandCount,
+			actualHandCount
+			);
+	assertTrue(expectedHandCount == actualHandCount, "TEST FAILED: hand count incorrect.\n");
 
 	free(state);
 }
 
 int main() {
-	testNumHandCards();
+	testPlayVillage();
 	printf("\n >>>>> SUCCESS: Testing complete <<<<<\n\n");
 	return 0;
 }
