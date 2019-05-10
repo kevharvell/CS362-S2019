4a7c372da79275892d44577dbb05d90143fe0bef
Kevin Harvell <kevharvell@gmail.com>
2019-05-09 22:00:34 -0500

    test

--- projects/harvellk/dominion/randomtestcard1.c
+++ projects/harvellk/dominion/randomtestcard1.c
@@ -61,7 +61,7 @@ int main() {
 	SelectStream(2);
 	PutSeed(3);
 
-	for (n = 0; n < 2000; n++) {
+	for (n = 0; n < 10; n++) {
 		for (i = 0; i < sizeof(struct gameState); i++) {
 			((char*)&G)[i] = floor(Random() * 256);
 		}
